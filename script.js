9c7d35430d66f64cd82c0efbea73b8807673a05e
shivamg-2410 <gohelshivam2410@gmail.com>
2025-07-15 10:37:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const toggleBtn = document.getElementById('themeToggle');
 toggleBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+
+if (!localStorage.getItem('visited')) {
+  alert("👋 Welcome to my portfolio!");
+  localStorage.setItem('visited', 'yes');
+}
+
+const copyBtn = document.getElementById('copyEmailBtn');
+const emailText = document.getElementById('emailText');
+
+copyBtn.addEventListener('click', () => {
+  navigator.clipboard.writeText(emailText.textContent).then(() => {
+    copyBtn.textContent = "✅ Copied!";
+    setTimeout(() => {
+      copyBtn.textContent = "📋 Copy";
+    }, 1500);
+  });
+});
